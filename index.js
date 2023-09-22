f07dda20b05027703e3da2ee6716a1e00f83a07f
Jack Henza <jackhenza@gmail.com>
2023-09-22 15:46:57 -0400

    goofy

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.getElementById("main").remove();
+const newHeader = document.createElement("h1");
+newHeader.textContent = "INSERT CHAMPION NAME HERE is the champion!";
+newHeader.id = "victory";
+document.body.append(newHeader);
